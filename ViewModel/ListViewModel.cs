0fb606181ddb8eddbd3bc852fbeaa611095692eb
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-06-17 19:00:33 -0500

    firstCommit
    Example with all propertys from lisview

---
+++ ViewModel/ListViewModel.cs
@@ -0,0 +1,53 @@
+﻿using ControlesTipoVista.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlesTipoVista.ViewModel
+{
+    public class ListViewModel :INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private List<Robot> _robots;
+
+        public List<Robot> Robots
+        {
+            get
+            {
+                return _robots;
+            }
+            set
+            {
+                _robots = value;
+                OnPropertyChanged(nameof(Robots));
+
+            }
+        }
+
+        public ListViewModel()
+        {
+            Robots = new List<Robot>
+            {
+                new Robot {Nombre = "Robot 1", Imagen = "dotnet_bot.png", Peso = 25.5},
+                new Robot {Nombre = "Robot 2", Imagen = "dotnet_bot.png", Peso = 10.5},
+                new Robot {Nombre = "Robot 3", Imagen = "dotnet_bot.png", Peso = 20.5},
+                new Robot {Nombre = "Robot 1", Imagen = "dotnet_bot.png", Peso = 25.5},
+                new Robot {Nombre = "Robot 2", Imagen = "dotnet_bot.png", Peso = 10.5},
+                new Robot {Nombre = "Robot 3", Imagen = "dotnet_bot.png", Peso = 20.5},
+                new Robot {Nombre = "Robot 1", Imagen = "dotnet_bot.png", Peso = 25.5},
+                new Robot {Nombre = "Robot 2", Imagen = "dotnet_bot.png", Peso = 10.5},
+                new Robot {Nombre = "Robot 3", Imagen = "dotnet_bot.png", Peso = 20.5},
+
+            };
+        }
+    }
+}
